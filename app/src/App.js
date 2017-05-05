3f55cd0429411a760c135507f187c812061d0d75
edoimamovic <eimamovic1@etf.unsa.ba>
2017-05-05 20:15:43 +0200

    inicijalna verzija početne stranice (samo izgled)

---
+++ app/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './LoginPage';
+import './LoginPage.css';
+
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+      <div className="background">
+        <div className="layer">
+        </div>
+      </div>
+      <LoginPage /> </div>
+
+    );
+
+/*    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );*/
+  }
+}
+
+export default App;
---
+++ app/src/LoginPage.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './LoginPage.css';
+
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <div className="LoginPage container-fluid">
+        <div className="row long30">
+          <div className="naslov">
+            <h1>Univerzitet u Sarajevu</h1>
+            <h2>Studentski portal</h2>
+          </div>
+        </div>
+        <div className="row kriticni">
+          <div className="col-sm-8">
+            <img className="logo-unsa" src={require('./img/unsa.png')} />
+          </div>
+          <div className="col-sm-3">
+
+            <form className="form-horizontal">
+              <div className="form-group">
+                
+                <div className="col-sm-12">
+                  <input type="text" className="form-control" placeholder="Ime"/>
+                </div>
+              </div>
+
+              <div className="form-group">
+                <div className="col-sm-12">
+                  <input type="text" className="form-control" placeholder="Prezime"/>
+                </div>
+              </div>
+    
+              <div className="form-group">
+                <div className="col-xs-9"></div>
+
+                <div className="col-xs-2">
+                  <input type="submit" className="btn btn-primary" value="Prijava" />
+                  </div>
+                </div>
+            </form>
+
+          </div>
+          <div className="col-sm-2"></div>
+        </div>
+
+        <div className="dno row">
+          <div className="col-sm-1"></div>
+          <div className="col-sm-1"><h2>ISSS</h2></div>
+          <div className="col-sm-6"></div>
+          <div className="col-sm-4"><h2>Univerzitet u Sarajevu</h2></div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
