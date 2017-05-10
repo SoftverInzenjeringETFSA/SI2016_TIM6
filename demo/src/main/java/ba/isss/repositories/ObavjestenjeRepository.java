e1496cfd45bda481b4d23e8941eb77fd6aa59878
ekalac1 <elzaa_95@hotmail.com>
2017-05-10 10:37:04 +0200

    Obavjestenja service done

--- demo/src/main/java/ba/isss/repositories/ObavjestenjeRepository.java
+++ demo/src/main/java/ba/isss/repositories/ObavjestenjeRepository.java
@@ -1,8 +1,13 @@
 package ba.isss.repositories;
 
 import ba.isss.models.Obavjestenje;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ObavjestenjeRepository extends PagingAndSortingRepository<Obavjestenje, Integer> {
 
+	long count();
+	boolean exists(Integer primaryKey);
+	Iterable<Obavjestenje> findAll();
+	Obavjestenje findOne(Integer id);
 }
---
+++ demo/src/main/java/ba/isss/services/ObavjestenjeService.java
@@ -0,0 +1,22 @@
+package ba.isss.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ba.isss.models.Obavjestenje;
+import ba.isss.repositories.ObavjestenjeRepository;
+
+@Service
+public class ObavjestenjeService {
+	
+	 @Autowired
+	    ObavjestenjeRepository repository;
+	     
+	    public Iterable<Obavjestenje> findAll() {
+	        return repository.findAll();
+	    }
+	    
+	    public Obavjestenje findOne(Integer id) {
+	    	return repository.findOne(id);
+		}
+}
