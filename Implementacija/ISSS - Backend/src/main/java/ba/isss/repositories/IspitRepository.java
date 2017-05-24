1bdf2168cfc2d03441f7bcf40532ba6596839ba0
sbecirovic <seila.becirovic@gmail.com>
2017-05-24 21:05:12 +0200

    Nemogucnost prijave na polozen predmet

--- Implementacija/ISSS - Backend/src/main/java/ba/isss/repositories/IspitRepository.java
+++ Implementacija/ISSS - Backend/src/main/java/ba/isss/repositories/IspitRepository.java
@@ -13,13 +13,13 @@ public interface IspitRepository extends Repository<Ispit, Integer> {
 	Iterable<Ispit> findAll();
 	Ispit findOne(Integer id);
 	
-	@Query("select i from Ispit i, Prijava p, Student s, Predmet r "
-			+ "where s.id = p.student.id and p.ispit.id = i.id and i.predmet.id = r.id and s.id = :id "
+	@Query("select i from Ispit i, Prijava p, Student s, Predmet r, Pohadjanje k "
+			+ "where s.id = p.student.id and p.ispit.id = i.id and i.predmet.id = r.id and s.id = :id and k.student.id = s.id and r.id = k.predmet.id and k.ocjena is null "
 			+ "and i.termin > current_date() order by i.termin desc")
 	Iterable<Ispit> findAllPrijavljeniForStudent(@Param("id") Integer id);
 	
 	@Query("select distinct i from Ispit i, Prijava p, Pohadjanje po, Student s, Predmet pred "
-			+ "where s.id = po.student.id and po.predmet.id = pred.id and pred.id = i.predmet.id "
+			+ "where s.id = po.student.id and po.predmet.id = pred.id  and po.ocjena is null and pred.id = i.predmet.id "
 			+ "and i.id not in ("
 			+ "select isp.id from Ispit isp, Prijava pr, Student st, Predmet pre "
 			+ "where st.id = pr.student.id and pr.ispit.id = isp.id and isp.predmet.id = pre.id and st.id = :id "
