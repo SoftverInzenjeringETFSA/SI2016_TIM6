85edc8d51b5072140a7d0839bf245b4d329f8250
Jasmina Bajramović <bajramovicjasmina2@gmail.com>
2017-05-24 02:19:39 +0200

    sredjen LoginPage

--- Implementacija/app/src/LoginPage.js
+++ Implementacija/app/src/LoginPage.js
@@ -34,50 +34,45 @@ class LoginPage extends Component {
             <div className="naslov text-center">
               <h1>Univerzitet u Sarajevu</h1>
               <h2>Studentski portal</h2>
+              <img className="logo-welcome" src={require('./img/unsa.png')} alt="UNSA Logo"/>
             </div>
           </div>
 
-          <div className="row">
-              <div className="hidden-xs hidden-sm col-md-4 col-md-offset-1">
-                <img className="v-center" src={require('./img/unsa.png')} alt="UNSA Logo"/>
-              </div>
+         
+          <div className="row text-center">
 
-              <form className="v-center form-horizontal col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3 col-md-3 col-md-offset-3"
+             <form className="v-center form-horizontal col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4 "
                     >
 
-                <div className="form-group">
-                    <input type="text" className="form-control" id="username" placeholder="Korisničko ime" onChange={this.onChangeUsername}  />
-                </div>
-
-                <div className="form-group">
-                    <input type="password" className="form-control" id="password"  placeholder="Šifra" onChange={this.onChangePassword}  />
-                </div>
+                <div className="login-input-wrapper">
+                  <div className="form-group has-feedback">
+                      <input className=" colors login-input" placeholder="Korisničko ime" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Korisničko ime'"
+                          type="text" onChange={this.onChangeUsername} />
+                      <i className="glyphicon glyphicon-user form-control-feedback pull-left" aria-hidden="true" ></i>
+                  </div>
+                  <div className="form-group has-feedback">
+                      <input className="colors login-input" placeholder="Lozinka" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Lozinka'"
+                          type="password" onChange={this.onChangePassword}/>
+                      <i className="glyphicon glyphicon-lock form-control-feedback pull-left" aria-hidden="true" ></i>
+                  </div>
+               </div>
                 {this.props.poruka
               ? <div className="v-center">
                 <div>
-                  <h4>Prijava nije uspjela.</h4>
+                  <h5 className="error-login">Prijava nije uspjela.</h5>
                 </div>
               </div>
               :
               null
               }
                 <div className="form-group">
-                  <button type="button" className="btn btn-primary" onClick={this.onLogin}>Prijava</button>
+                  <button type="button" className="btn btn-primary btn-outline btn-login" onClick={this.onLogin}>PRIJAVITE SE</button>
                 </div>
 
               </form>
           </div>
 
-          <div className="navbar-fixed-bottom dno row">
-            <div className="hidden-xs col-sm-10 col-sm-offset-1">
-              <h1 className="row footer-wrapper">
-                <span><strong>ISSS</strong></span><span>Univerzitet u Sarajevu</span>
-              </h1>
-            </div>
-            <div className="hidden-sm hidden-md hidden-lg col-xs-10 col-xs-offset-1">
-              <h1 className="text-center"><strong>ISSS</strong></h1>
-            </div>
-          </div>
+          
         </div>
       </div>
     );
