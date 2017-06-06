3632fbeb8019094d56109dc67f6855fed5dc491b
Edin Begić <ebegic2@etf.unsa.ba>
2017-06-06 19:47:14 +0200

    Fixed bugs #6 , #9

--- Implementacija/ISSS - Backend/src/main/java/ba/isss/services/PrijavaService.java
+++ Implementacija/ISSS - Backend/src/main/java/ba/isss/services/PrijavaService.java
@@ -1,6 +1,9 @@
 package ba.isss.services;
 
+import ba.isss.models.Pohadjanje;
+import ba.isss.models.Predmet;
 import ba.isss.models.Student;
+import ba.isss.repositories.PohadjanjeRepository;
 import ba.isss.repositories.PrijavaRepository;
 import ba.isss.models.Prijava;
 
@@ -16,7 +19,8 @@ public class PrijavaService {
 	private PrijavaRepository prijavaRepo;
 	@Autowired
 	private PrijavaService prijavaService;
-	
+	@Autowired
+    private PohadjanjeService pohadjanjeService;
 	public Iterable<Prijava> findAllByStudentID(Integer id) {
 		return prijavaRepo.findAllByStudentId(id);
 	}
@@ -26,15 +30,22 @@ public class PrijavaService {
 	    if(!p.getStudent().equals(s))
 	        throw new Exception("ERROR");
 		Iterable<Prijava> prijave = prijavaService.findAllByStudentID(p.getStudent().getId());
-		
+		Iterable<Pohadjanje> pohadjanja = pohadjanjeService.findAllByStudent(s.getId());
 		for (Prijava prijava : prijave) {
 			if(p.getStudent().getId() == prijava.getStudent().getId() && p.getIspit().getPredmet().getId() == prijava.getIspit().getPredmet().getId()) {
 				throw new IllegalArgumentException("Prijava za ispit postoji");
 			}
 		}
-		
-		
-		
+		int brojac = 0;
+		for (Pohadjanje pohadjanje : pohadjanja)
+        {
+            if(p.getIspit().getPredmet() == pohadjanje.getPredmet())
+                brojac++;
+            if(p.getIspit().getPredmet() == pohadjanje.getPredmet() && pohadjanje.getOcjena() != null)
+                throw new IllegalArgumentException("Student je već upisao ocjenu iz datog predmeta");
+        }
+		if(brojac == 0)
+		    throw  new IllegalArgumentException("Student se ne može prijaviti na predmet koji ne sluša");
 		
 		Date prijave_do = p.getIspit().getPrijave_do();
 		Date trenutno = new Date(System.currentTimeMillis());
